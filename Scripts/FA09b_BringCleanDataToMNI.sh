e2cf8c54abb609040ba20c73a92df0d53198c2a0
javiergcas <javier.gonzalez-castillo@nih.gov>
2020-08-17 11:35:46 -0400

    Added script to concatenate fully preprocessed datasets in MNI space

---
+++ Scripts/FA09b_BringCleanDataToMNI.sh
@@ -0,0 +1,6 @@
+cd /data/SFIM_FlipAngle/PRJ_FAConn/PrcsData/ALL_CorrMaps
+
+3dTcat -overwrite -tr 2.5 -prefix ALL_fMRI_015.errts.tproject.nii.gz ../P???/D03_Preproc_NoRICOR_FIR_015/errts.P???_015.tproject.MNI.nii.gz 
+3dTcat -overwrite -tr 2.5 -prefix ALL_fMRI_050.errts.tproject.nii.gz ../P???/D03_Preproc_NoRICOR_FIR_050/errts.P???_050.tproject.MNI.nii.gz 
+3dTcat -overwrite -tr 2.5 -prefix ALL_fMRI_077.errts.tproject.nii.gz ../P???/D03_Preproc_NoRICOR_FIR_077/errts.P???_077.tproject.MNI.nii.gz 
+3dTcat -overwrite -tr 2.5 -prefix ALL_fMRI_090.errts.tproject.nii.gz ../P???/D03_Preproc_NoRICOR_FIR_090/errts.P???_090.tproject.MNI.nii.gz
